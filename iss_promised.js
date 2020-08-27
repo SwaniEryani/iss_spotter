b58a44735b001df2890a3f19ceb4b16f0ef1b706
SwaniEryani <aleryanisilwan@gmail.com>
2020-08-27 22:27:17 +0000

    catch error

--- iss_promised.js
+++ iss_promised.js
@@ -3,7 +3,7 @@ const request = require('request-promise-native');
 const fetchMyIP = function() {
   return request('https://api.ipify.org?format=json');
 };
-const fetchCoordsByIP = function body) {
+const fetchCoordsByIP = function (body) {
   const ip = JSON.parse(body).ip;
   return request(`https://ipvigilante.co/json/${ip}`);
 };
